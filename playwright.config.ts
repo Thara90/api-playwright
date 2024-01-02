ace52723828c605443e08eca06915fafb91b3069
Thara90 <tpshadinijk@gmail.com>
2024-01-02 20:17:24 +0530

    enhance test coverage in GET requests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-import { config } from "dotenv";
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -13,13 +12,13 @@ export default defineConfig({
   globalSetup: 'utils/globalSetup.ts',
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- tests/booking.get.spec.ts
+++ tests/booking.get.spec.ts
@@ -3,30 +3,51 @@ import { test, expect } from '@playwright/test';
 test.describe('booking/ GET requests', () => {
     let cookies: string = "";
 
-    test.beforeAll( async ({ request }) => {
-        const response = await request.post("auth/login",{
-            data : {
-                username : "admin",
-                password : "password",
+    test.beforeAll(async ({ request }) => {
+        const response = await request.post("auth/login", {
+            data: {
+                username: "admin",
+                password: "password",
             },
         });
         expect(response.status()).toBe(200);
         const headers = await response.headers();
         cookies = headers["set-cookie"];
-        console.log(cookies);
-
     });
 
-    test('Get booking summary', async ({ request }) => {
+    //************** booking summary **************//
+    test('Get booking summary with specific room id', async ({ request }) => {
         const response = await request.get("booking/summary?roomid=1");
+
         expect(response.status()).toBe(200);
+
         const body = await response.json();
         console.log(JSON.stringify(body));
-        //console.log(body.bookings.length);
         expect(body.bookings.length).toBeGreaterThanOrEqual(1);
     });
 
-    test('Get all bookings', async ({ request }) => {
+    test('Get booking summary with room id does not exists', async ({ request }) => {
+        const response = await request.get("booking/summary?roomid=1000");
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+        console.log(JSON.stringify(body));
+        expect(body.bookings.length).toBeGreaterThanOrEqual(0);
+    });
+
+    test('Get booking summary when room id is empty', async ({ request }) => {
+        const response = await request.get("booking/summary?roomid=");
+
+        expect(response.status()).toBe(500);
+        const body = await response.json();
+        console.log(JSON.stringify(body));
+        expect(body.status).toBe(500);
+        expect(body.error).toBe("Internal Server Error");
+    });
+
+    //************** get all bookings **************//
+    test('Get all bookings with authentication', async ({ request }) => {
         const response = await request.get("booking/", {
             headers: { cookie: cookies },
         });
@@ -37,7 +58,16 @@ test.describe('booking/ GET requests', () => {
         expect(body.bookings.length).toBeGreaterThanOrEqual(1);
     });
 
-    test('Get a booking with ID', async ({ request }) => {
+    test('Get all bookings without authentication', async ({ request }) => {
+        const response = await request.get("booking/");
+
+        expect(response.status()).toBe(403);
+        const body = await response.text();
+        expect(body).toBe("");
+    });
+
+    //************** get booking by id **************//
+    test('Get a booking by id', async ({ request }) => {
         const response = await request.get("booking/1", {
             headers: { cookie: cookies },
         });
@@ -50,6 +80,26 @@ test.describe('booking/ GET requests', () => {
         expect(body.firstname).toBe("James");
         expect(body.lastname).toBe("Dean");
         expect(body.depositpaid).toBe(true);
+    });
+
+    test('Get a booking by id that does not exist', async ({ request }) => {
+        const response = await request.get("booking/1000", {
+            headers: { cookie: cookies },
+        });
+
+        expect(response.status()).toBe(404);
+
+        const body = await response.text();
+        expect(body).toBe("");
+    });
+
+    test('Get a booking by id without authentication', async ({ request }) => {
+        const response = await request.get("booking/1000");
+
+        expect(response.status()).toBe(403);
+
+        const body = await response.text();
+        expect(body).toBe("");
     });
 
 });
