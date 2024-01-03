91049232ac9524f625d2b86947eded51080a0544
Thara90 <tpshadinijk@gmail.com>
2024-01-03 18:46:23 +0530

    enhance POST req

--- tests/booking.post.spec.ts
+++ tests/booking.post.spec.ts
@@ -1,33 +1,93 @@
 import { test, expect } from '@playwright/test';
-import {Booking} from '../lib/datafactory/booking'
+import { Booking } from '../lib/datafactory/booking'
+import * as createBookingReq from '../req-json/createBooking.json';
 
 test.describe('booking/ POST requests', () => {
-const booking = new Booking();
+    const booking = new Booking();
+    let roomID, checkin, checkout, firstName, lastName, userEmail, phoneNumber;
 
-    test.only('Create a booking', async ({ request }) => {
+    test.beforeEach(async () => {
+        roomID = booking.generateRandomRoomID();
+        checkin = booking.generateRandomDates().checkin;
+        checkout = booking.generateRandomDates().checkout;
+        firstName = booking.generateRandomUserDetails().firstName;
+        lastName = booking.generateRandomUserDetails().lastName;
+        userEmail = booking.generateRandomUserDetails().userEmail;
+        phoneNumber = booking.generateRandomUserDetails().phoneNumber;
+    });
+
+    test('Create a booking with all inputs', async ({ request }) => {
+        const BookingReq = {
+            ...createBookingReq,
+            roomid: roomID,
+            firstname: firstName,
+            lastname: lastName,
+            depositpaid: "true",
+            email: userEmail,
+            phone: phoneNumber,
+            bookingdates: {
+                checkin: checkin,
+                checkout: checkout
+            }
+        };
         const response = await request.post("booking/",
             {
-                data: {
-                    "bookingid": 12,
-                    "roomid": 12,
-                    "firstname": "test",
-                    "lastname": "user",
-                    "depositpaid": true,
-                    "email": "test@mailinator.com",
-                    "phone": "071112333123",
-                    "bookingdates": {
-                        "checkin": "2024-12-19",
-                        "checkout": "2024-12-22"
-                    }
-                }
+                data: BookingReq
             });
 
-        expect(response.status()).toBe(201);
+        console.log(roomID, firstName, lastName, userEmail, phoneNumber, checkin, checkout);
+        expect.soft(response.status()).toBe(201);
         const body = await response.json();
-        expect(body.booking.bookingid).toBe(body.bookingid);
-        expect(body.booking.firstname).toBe("test");
-        expect(body.booking.lastname).toBe("user");
-
+        console.log(JSON.stringify(body));
     });
+
+    test('Create a booking without required inputs', async ({ request }) => {
+        const BookingReq = {
+            ...createBookingReq,
+            roomid: roomID,
+            email: userEmail,
+            phone: phoneNumber,
+            bookingdates: {
+                checkin: checkin,
+                checkout: checkout
+            }
+        };
+        const response = await request.post("booking/",
+            {
+                data: BookingReq
+            });
+
+        console.log(roomID, firstName, lastName, userEmail, phoneNumber, checkin, checkout);
+
+        expect.soft(response.status()).toBe(400);
+        const body = await response.json();
+        console.log(JSON.stringify(body));
+    });
+
+    // test('Create a booking with only required inputs', async ({ request }) => {
+    //     const BookingReq = {
+    //         ...createBookingReq,
+    //         roomid: roomID,
+    //         firstname: firstName,
+    //         lastname: lastName,
+    //         depositpaid: "true",
+    //         bookingdates: {
+    //             checkin: checkin,
+    //             checkout: checkout
+    //         }
+    //     };
+    //     const response = await request.post("booking/",
+    //         {
+    //             data: BookingReq
+    //         });
+
+    //     console.log(roomID, firstName, lastName, userEmail, phoneNumber, checkin, checkout);
+
+    //     expect.soft(response.status()).toBe(201);
+    //     const body = await response.json();
+    //     console.log(JSON.stringify(body));
+
+    // });
+
 
 });
